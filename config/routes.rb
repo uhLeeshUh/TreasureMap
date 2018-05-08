9822c9e07c1d35188a305eae69fcf46ab975ccfb
AliciaUnderhill <alicia.underhill@gmail.com>
2018-05-08 18:09:00 -0400

    Create SessionsController and associated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :user, only: [:create, :show]
+    resource :session, only: [:create, :destroy]
   end
 
 end
