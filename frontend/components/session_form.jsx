97829070700af67612bb0bab4a5ab47cad97194d
AliciaUnderhill <alicia.underhill@gmail.com>
2018-05-09 12:16:48 -0400

    Add site background image

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -36,30 +36,28 @@ class SessionForm extends React.Component {
     }
 
     return (
-      <div className="session">
+      <main className="session">
         <div className="session-form">
           <h1 className="session-title">{this.props.headerText}</h1>
           <div className="altText">
             {this.props.alternateText}
               <Link className="link" to={this.props.linkedRoute}>{this.props.alternateLinkText}</Link>
-            <p>
-              {this.props.errors}
-            </p>
+            <p>{this.props.errors}</p>
           </div>
           <form onSubmit={this.submit} className="form-body">
             <label className="session-label">USERNAME
             <input className="username" type="text" value={this.state.username} onChange={this.handleChange("username")} />
             </label>
-            <br></br>
+
             <label className="session-label">PASSWORD
             <input className="password" type="password" value={this.state.password} onChange={this.handleChange("password")} />
             </label>
-            <br></br>
+          
             {avatar}
             <button className="session-button">{this.props.buttonText}</button>
           </form>
         </div>
-      </div>
+      </main>
     );
   }
   // how do I handle the avatar input field?
