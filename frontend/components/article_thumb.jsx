73da51ae1170dc8dbdea7509bfd863989a584bb1
AliciaUnderhill <alicia.underhill@gmail.com>
2018-05-15 15:01:31 -0400

    Finish spacing and editing of article thumbs index

--- frontend/components/article_thumb.jsx
+++ frontend/components/article_thumb.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ArticleThumb = (props) => {
+
   return (
-    <section className="article-thumb col-1-4">
-      <img className="article-thumb-image" src={props.image.image_url}></img>
-      <p className="article-thumb-count">{props.count}</p>
-      <ul className="article-thumb-summary">
-        <li>{props.city.name}</li>
-        <li>{props.article.name}</li>
-        <li>{props.article.description}</li>
-      </ul>
-    </section>
+    <div className="thumb-holder col-1-4">
+      <Link to={`/articles/${props.article.id}`}>
+        <section className="article-thumb">
+          <img className="article-thumb-image" src={props.image.image_url}></img>
+          <div className="article-thumb-count">
+            <p>{props.count}</p>
+          </div>
+          <ul className="article-thumb-summary">
+            <li className="thumb-city-name">{props.city.name}</li>
+            <li className="thumb-article-name">{props.article.name}</li>
+            <li className="thumb-article-desc">{props.article.description}</li>
+          </ul>
+        </section>
+        <div className="thumb-border"></div>
+      </Link>
+    </div>
   );
 };
 
 export default ArticleThumb;
+
+
+
+
+
+
+
+// const imageFill = {
+//   backgroundImage: `url(${props.image.image_url})`
+// };
+
+// <div className="article-thumb-image" style={imageFill}></div>
