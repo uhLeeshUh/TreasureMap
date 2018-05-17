96b9737626a5ad57deefb6560cdb4867758b417e
AliciaUnderhill <alicia.underhill@gmail.com>
2018-05-16 23:32:51 -0400

    Complete design of splash page and fix editor hover names on article show

--- frontend/components/featured_article.jsx
+++ frontend/components/featured_article.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ArticleThumb from './article_thumb';
 import { fetchArticle } from '../actions/article_actions';
+import { Link } from 'react-router-dom';
 
 class FeaturedArticle extends React.Component {
   constructor(props){
@@ -18,10 +19,19 @@ class FeaturedArticle extends React.Component {
     }
 
     return (
-      <section>
-        <h1>{this.props.article.name}</h1>
-        <img></img>
-      </section>
+      <main className="feature-article-holder">
+        <div className="feature-article-headline-holder">
+          <h1 className="feature-article-header">Today's Featured Find</h1>
+        </div>
+        <Link className="feature-article-link" to={`/articles/${this.props.article.id}`}>
+          <section className="feature-article-content">
+            <img key={this.props.image.id}className='feature-article-image' src={this.props.image.image_url}></img>
+            <h3 className="feature-article-city">{this.props.city.name}</h3>
+            <h1 className="feature-article-title">{this.props.article.name}</h1>
+            <p className="feature-article-description">{this.props.article.description}</p>
+          </section>
+        </Link>
+      </main>
     );
   }
 }
@@ -29,7 +39,7 @@ class FeaturedArticle extends React.Component {
 const mapStateToProps = (state) => {
   let article;
   let city;
-  let images;
+  let image;
   let articleInState = true;
 
   if (!state.entities.articles[1]){
@@ -40,14 +50,12 @@ const mapStateToProps = (state) => {
 
   article = state.entities.articles[1];
   city = state.entities.cities[article.city_id];
-  images = article.image_ids.map(image_id => {
-    return state.entities.images[image_id];
-  });
+  image = state.entities.images[article.image_ids[0]];
 
   return {
     article,
     city,
-    images,
+    image,
     articleInState
   };
 
