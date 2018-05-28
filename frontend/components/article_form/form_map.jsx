a131c391bcf574f642b653435ce5a114a866a672
Alicia <alicia.underhill@gmail.com>
2018-05-28 17:38:47 -0400

    Initialize autocomplete dropdown

--- frontend/components/article_form/form_map.jsx
+++ frontend/components/article_form/form_map.jsx
@@ -2,8 +2,34 @@
   import ReactDOM from 'react-dom';
 
   class FormMap extends React.Component{
+    constructor(props){
+      super(props);
+    }
+
+    componentDidMount(){
+      this.createAutoComplete();
+    }
+
+    //create search bar
+    //create map to view location chosen
+    //needs to return lat/lng coordinates as a callback to the parent to store in formData
+
+    createAutoComplete(){
+      const autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementById('autocomplete')),
+        {types: ['geocode']}
+      );
+
+    }
+
     render(){
-      return "Hellow werld!";
+      return (
+        <section>
+          <div>
+            <input id="autocomplete" placeholder="E.g. 304 Deerfield Way, Normal, Illinois" type="text" ></input>
+          </div>
+        </section>
+      );
     }
   }
 
