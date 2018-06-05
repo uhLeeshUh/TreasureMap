6d1e430cf13937bab18f3989c190c04e3054663a
Alicia <alicia.underhill@gmail.com>
2018-06-05 15:42:33 -0400

    Update cities reducer to accomodate fetching featured articles

--- frontend/reducers/entities/cities_reducer.js
+++ frontend/reducers/entities/cities_reducer.js
@@ -1,5 +1,5 @@
 import { RECEIVE_CITIES, RECEIVE_CITY, RECEIVE_TOP_CITIES } from '../../actions/city_actions';
-import { RECEIVE_ARTICLE, RECEIVE_RANDOM_ARTICLE } from '../../actions/article_actions';
+import { RECEIVE_ARTICLE, RECEIVE_RANDOM_ARTICLES } from '../../actions/article_actions';
 import { RECEIVE_COUNTRY } from '../../actions/country_actions';
 import { merge } from 'lodash';
 
@@ -10,16 +10,23 @@ const citiesReducer = (state = {}, action) => {
     case RECEIVE_TOP_CITIES:
       let newState = merge({}, state, action.citiesPayload);
       return newState;
+
     case RECEIVE_CITY:
       newState = merge({}, state, {[action.cityPayload.city.id]: action.cityPayload.city});
       return newState;
+
     case RECEIVE_ARTICLE:
-    case RECEIVE_RANDOM_ARTICLE:
       newState = merge({}, state, {[action.articlePayload.city.id]: action.articlePayload.city});
       return newState;
+
+    case RECEIVE_RANDOM_ARTICLES:
+      newState = merge({}, state, action.articlesPayload.cities);
+      return newState;
+
     case RECEIVE_COUNTRY:
       newState = merge({}, state, action.countryPayload.cities);
       return newState;
+
     default:
       return state;
   }
