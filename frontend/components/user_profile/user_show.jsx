f84a66bd256d79c454d02eba5fe55f5d8ab45249
Alicia <alicia.underhill@gmail.com>
2018-06-13 16:16:05 -0400

    Begin user profile skeleton

---
+++ frontend/components/user_profile/user_show.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const UserShow = props => {
+  return (
+    <main>
+      <section>
+        <img> </img>
+        <h3>username goes here</h3>
+      </section>
+      <section>
+        <h3>user stats</h3>
+        <ul>
+          <li># articles written</li>
+          <li># articles edited</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Articles written</h3>
+        <ul>
+          <li>Title of each article...</li>
+          <li>Title of each article...</li>
+          <li>Title of each article...</li>
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+// ImageIndex:
+// render() { return <ul>{images}</ul>}
+export default UserShow;
