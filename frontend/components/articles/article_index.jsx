0dc43268428a5d4e83079d4161ab5a3cc2886bc1
Alicia <alicia.underhill@gmail.com>
2018-06-13 16:47:15 -0400

    Include shell of user profile

---
+++ frontend/components/articles/article_index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ArticleThumb from '../article_thumb';
+
+const ArticleIndex = (props) => {
+
+  // const articleThumbs = props.articles.map(article => {
+  //
+  // });
+
+  // {articleThumbs}
+  return (
+    <ul>
+      <li>Article 1 Info Here</li>
+      <li>Article 2 Info Here</li>
+      <li>Article 3 Info Here</li>
+    </ul>
+  );
+};
+
+export default ArticleIndex;
